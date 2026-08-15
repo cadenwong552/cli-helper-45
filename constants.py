b273ce94509acf6ce560f8e9d1d4854d8d801185
Julian Wong <cadenwong552@gmail.com>
2026-08-16 00:30:04 +0200

    add game constants defined

--- constants.py
+++ constants.py
@@ -1,37 +1,29 @@
-import json
-import os
-
-class ConfigLoader:
-    DEFAULT_CONFIG = {
-        'volume': 50,
-        'difficulty': 'normal',
-        'screen_resolution': '1920x1080',
-        'fullscreen': True,
-        'key_bindings': {
-            'move_forward': 'W',
-            'move_backward': 'S',
-            'turn_left': 'A',
-            'turn_right': 'D'
-        }
-    }
-
-    def __init__(self, config_file='config.json'):
-        self.config_file = config_file
-        self.config = self.load_config()
-
-    def load_config(self):
-        if os.path.exists(self.config_file):
-            with open(self.config_file, 'r') as f:
-                user_config = json.load(f)
-            return {**self.DEFAULT_CONFIG, **user_config}
-        return self.DEFAULT_CONFIG
-
-    def get(self, key):
-        return self.config.get(key, None)
-
-    def set(self, key, value):
-        self.config[key] = value
-
-    def save(self):
-        with open(self.config_file, 'w') as f:
-            json.dump(self.config, f, indent=4)
+SCREEN_RESOLUTION = (1920, 1080)
+FPS_LIMIT = 60
+KEYBINDS = {
+    'MOVE_UP': 'W',
+    'MOVE_DOWN': 'S',
+    'MOVE_LEFT': 'A',
+    'MOVE_RIGHT': 'D',
+    'JUMP': 'SPACE',
+    'SHOOT': 'LEFT_MOUSE',
+}
+LEVELS = [
+    'tutorial',
+    'easy',
+    'medium',
+    'hard',
+    'nightmare',
+]
+ENEMY_TYPES = {
+    'GOBLIN': {'health': 30, 'damage': 5},
+    'TROLL': {'health': 100, 'damage': 15},
+    'DRAGON': {'health': 300, 'damage': 50},
+}
+MAX_INVENTORY_SIZE = 20
+POWER_UPS = [
+    'SPEED_BOOST',
+    'HEALTH_PACK',
+    'DAMAGE_BOOST',
+]
+VOLUME_LEVELS = [0.0, 0.25, 0.5, 0.75, 1.0]
