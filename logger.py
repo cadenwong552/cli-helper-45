73860efc6703363c9acd27a5a59548442d0d7996
Julian Wong <cadenwong552@gmail.com>
2026-08-19 00:15:02 +0200

    fix logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,26 +1,20 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
-class CustomFormatter(logging.Formatter):
-    # Custom log formatting
-    def format(self, record):
-        log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-def setup_logger(name):
-    # Setting up the logger
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    ch.setFormatter(CustomFormatter())
-    logger.addHandler(ch)
+def setup_logger(log_file='game.log', level=logging.INFO):
+    logger = logging.getLogger('game_logger')
+    logger.setLevel(level)
+    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=5)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
 
 if __name__ == '__main__':
-    log = setup_logger('game_logger')
+    log = setup_logger()
+    log.info('Logger is set up successfully!')
     log.debug('This is a debug message')
-    log.info('This is an info message')
     log.warning('This is a warning message')
     log.error('This is an error message')
     log.critical('This is a critical message')
