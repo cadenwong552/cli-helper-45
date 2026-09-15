c2c636a1106f4871ad319cffe34fe5c05ebcd100
Julian Wong <cadenwong552@gmail.com>
2026-09-15 02:05:22 +0200

    memoization layer for expensive game calculations

--- helpers.py
+++ helpers.py
@@ -1,33 +1,38 @@
-import re
+import functools
+import time
+import collections
 
-class GameInputSanitizer:
-    def __init__(self, allowed_commands=None):
-        self.commands = allowed_commands or {'play', 'quit', 'save', 'load', 'stats'}
+class PerformanceCache:
+    def __init__(self, max_size=128):
+        self.cache = collections.OrderedDict()
+        self.max_size = max_size
 
-    def validate(self, user_input):
-        raw = str(user_input).strip().lower()
-        if not raw:
-            return None
-        
-        token = re.split(r'\s+', raw)[0]
-        if token in self.commands:
-            return token
-        return None
+    def memoize_with_ttl(self, ttl=5):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                key = (args, tuple(sorted(kwargs.items())))
+                now = time.time()
+                if key in self.cache:
+                    result, timestamp = self.cache[key]
+                    if now - timestamp < ttl:
+                        return result
+                    del self.cache[key]
+                
+                result = func(*args, **kwargs)
+                self.cache[key] = (result, now)
+                if len(self.cache) > self.max_size:
+                    self.cache.popitem(last=False)
+                return result
+            return wrapper
+        return decorator
 
-def input_loop(sanitizer):
-    while True:
-        try:
-            user_text = input('>> ')
-            cmd = sanitizer.validate(user_text)
-            if cmd:
-                yield cmd
-            else:
-                print(f'Unknown sequence: {user_text[:10]}')
-        except (EOFError, KeyboardInterrupt):
-            break
+def heavy_game_calculation(n):
+    return sum(i * i for i in range(n))
 
-if __name__ == '__main__':
-    # usage example within cli-helper-45 game loop
-    validator = GameInputSanitizer()
-    for command in input_loop(validator):
-        print(f'Executing action: {command}')
+processor = PerformanceCache(max_size=256)
+
+@processor.memoize_with_ttl(ttl=30)
+def optimized_game_stat_fetcher(level_id):
+    # Simulate expensive IO or complex math logic
+    return heavy_game_calculation(level_id * 1000)
